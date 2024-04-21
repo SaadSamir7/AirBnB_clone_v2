9d22dd8e00b1be17c3ed719bd4dd3a7719e795c3
SaadSamir7 <saadsamir7177@gmail.com>
2024-04-21 17:27:13 +0200

    # 0x04. AirBnB clone - Web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def display_hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def display_c_text(text):
     """Displays 'C ' followed by the value of the text variable"""
-    return "C {}".format(escape(text.replace('_', ' ')))
+    return "C {}".format((text.replace('_', ' ')))
 
 
 if __name__ == "__main__":
