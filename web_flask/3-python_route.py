709b4bfb733fd01566c6e1ca4f5b8f4bf2b6631e
SaadSamir7 <saadsamir7177@gmail.com>
2024-04-21 17:48:06 +0200

    # 0x04. AirBnB clone - Web framework

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,12 +26,14 @@ def c_text(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-@app.route('/python/', strict_slashes=False)
+
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+def python(text='is cool'):
+    """Return a string when /python/<text> is called"""
+    message = text.replace("_", " ")
+    return "Python {}".format(message)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
