db9161b4cbaf7ddcd4cc0b49104c36d8ee7cd88f
SaadSamir7 <saadsamir7177@gmail.com>
2024-04-28 23:23:55 +0300

    # AirBnB Clone - The Console

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,16 @@
-#!/usr/bin/python3
-"""
-initialize the models package
-"""
+import os
 
-from os import getenv
+# Check the value of the environment variable
+storage_type = os.getenv('HBNB_TYPE_STORAGE')
 
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
+# Create an instance of the appropriate storage class based on the value
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
+# Execute the reload method after instantiation
 storage.reload()
